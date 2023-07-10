7388a08aae9d42fd8d04b02e0997370c5c0b9003
dirty g <you@example.com>
2023-07-10 19:14:48 +0300

    clean up

--- utils/removeAttributes.js
+++ utils/removeAttributes.js
@@ -9,31 +9,34 @@ let removeValue = [ "None", "None 1", "None 2", "None 3", "None 4", "None 5", "N
 let removeTraitType = [] //Enter a Traits you want to remove here. (ie: "Head")
 
 const main = () => {
-  // Read json data
-  let rawdata = fs.readFileSync(`${basePath}/build/json/_metadata.json`);
-  let data = JSON.parse(rawdata);
+  if (fs.existsSync(`${basePath}/build/json/_metadata.json`)) {
+    // Read json data
+    let rawdata = fs.readFileSync(`${basePath}/build/json/_metadata.json`);
+    let data = JSON.parse(rawdata);
 
-  // Create new directory if it doesn't already exist
-  const dir = `${basePath}/build/json`;
-  if (!fs.existsSync(dir)) {
-    fs.mkdirSync(dir, {
-      recursive: true
+    // Create new directory if it doesn't already exist
+    const dir = `${basePath}/build/json`;
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir, {
+        recursive: true
+      });
+    }
+
+    data.forEach((item) => {
+      removeValue.forEach((traitValue) => {
+        let newValue = item.attributes.filter(obj=> obj.value !== traitValue);
+        item.attributes = newValue;
+      })
+      removeTraitType.forEach((traitType) => {
+        let newValue = item.attributes.filter(obj=> obj.trait_type !== traitType);
+        item.attributes = newValue;
+      })
+      fs.writeFileSync(`${basePath}/build/json/${item.id}.json`, JSON.stringify(item, null, 2));
     });
+
+    fs.writeFileSync(`${basePath}/build/json/_metadata.json`, JSON.stringify(data, null, 2));
   }
-
-  data.forEach((item) => {
-    removeValue.forEach((traitValue) => {
-      let newValue = item.attributes.filter(obj=> obj.value !== traitValue);
-      item.attributes = newValue;
-    })
-    removeTraitType.forEach((traitType) => {
-      let newValue = item.attributes.filter(obj=> obj.trait_type !== traitType);
-      item.attributes = newValue;
-    })
-    fs.writeFileSync(`${basePath}/build/json/${item.id}.json`, JSON.stringify(item, null, 2));
-  });
-
-  fs.writeFileSync(`${basePath}/build/json/_metadata.json`, JSON.stringify(data, null, 2));
 }
 
 exports.default = main
+main()
--- utils/renameAttributes.js
+++ utils/renameAttributes.js
@@ -4,6 +4,7 @@ const path = require('path');
 const isLocal = typeof process.pkg === 'undefined';
 const basePath = isLocal ? process.cwd() : path.dirname(process.execPath);
 const fs = require('fs');
+const chalk = require('chalk')
 
 let valueBefore = ["3", "Layer", "Gola", "Dope Soda", "Lion Slippers"] //Enter old values here
 let valueAfter = [":3", ":|", "Gõla", "Dope", "Lion"] //Enter new values here
@@ -11,49 +12,50 @@ let traitTypeBefore = [] //Enter old trait_types here
 let traitTypeAfter = [] //Enter new trait_trypes here
 
 const main = () => {
-  // Read json data
-  let rawdata = fs.readFileSync(`${basePath}/build/json/_metadata.json`);
-  let data = JSON.parse(rawdata);
+  if (fs.existsSync(`${basePath}/build/json/_metadata.json`)) {
+      // Read json data
+    let rawdata = fs.readFileSync(`${basePath}/build/json/_metadata.json`);
+    let data = JSON.parse(rawdata);
 
-  // Create new directory if it doesn't already exist
-  const dir = `${basePath}/build/json`;
-  if (!fs.existsSync(dir)) {
-    fs.mkdirSync(dir, {
-      recursive: true
-    });
-  }
+    // Create new directory if it doesn't already exist
+    const dir = `${basePath}/build/json`;
+    if (!fs.existsSync(dir)) fs.mkdirSync(dir, { recursive: true })
 
-  if (valueBefore.length !== valueAfter.length) {
-    throw new Error(`Arrays must have the same number of items! valueBefore count (${valueBefore.length}) must match valueAfter count (${valueAfter.length})`)
-  }
+    if (valueBefore.length !== valueAfter.length) {
+      console.log(chalk.bold.red(`Arrays must have the same number of items! valueBefore count (${valueBefore.length}) must match valueAfter count (${valueAfter.length})`))
+      return
+    }
 
-  if (traitTypeBefore.length !== traitTypeAfter.length) {
-    throw new Error(`Arrays must have the same number of items! traitTypeBefore count (${traitTypeBefore.length}) must match traitTypeAfter count (${traitTypeAfter.length})`)
-  }
+    if (traitTypeBefore.length !== traitTypeAfter.length) {
+      console.log(chalk.bold.red(`Arrays must have the same number of items! traitTypeBefore count (${traitTypeBefore.length}) must match traitTypeAfter count (${traitTypeAfter.length})`))
+      return
+    }
 
-  data.forEach((item) => {
-    let attributes = item.attributes;
-    attributes.forEach((attribute) => {
-      // Update values
-      for (let i = 0; i < valueBefore.length; i++) {
-        if (attribute.value === valueBefore[i]) {
-          let updatedValue = attribute.value.replace(valueBefore[i], valueAfter[i]);
-          attribute.value = updatedValue;
+    data.forEach((item) => {
+      let attributes = item.attributes;
+      attributes.forEach((attribute) => {
+        // Update values
+        for (let i = 0; i < valueBefore.length; i++) {
+          if (attribute.value === valueBefore[i]) {
+            let updatedValue = attribute.value.replace(valueBefore[i], valueAfter[i]);
+            attribute.value = updatedValue;
+          }
         }
-      }
-      // Update trait_types
-      for (let i = 0; i < traitTypeBefore.length; i++) {
-        if (attribute.trait_type === traitTypeBefore[i]) {
-          let updatedTraitType = attribute.trait_type.replace(traitTypeBefore[i], traitTypeAfter[i]);
-          attribute.trait_type = updatedTraitType;
+        // Update trait_types
+        for (let i = 0; i < traitTypeBefore.length; i++) {
+          if (attribute.trait_type === traitTypeBefore[i]) {
+            let updatedTraitType = attribute.trait_type.replace(traitTypeBefore[i], traitTypeAfter[i]);
+            attribute.trait_type = updatedTraitType;
+          }
         }
-      }
+      });
+
+      fs.writeFileSync(`${basePath}/build/json/${item.id}.json`, JSON.stringify(item, null, 2));
     });
 
-    fs.writeFileSync(`${basePath}/build/json/${item.id}.json`, JSON.stringify(item, null, 2));
-  });
-
-  fs.writeFileSync(`${basePath}/build/json/_metadata.json`, JSON.stringify(data, null, 2));
+    fs.writeFileSync(`${basePath}/build/json/_metadata.json`, JSON.stringify(data, null, 2));
+  }
 }
 
 exports.default = main
+main()
